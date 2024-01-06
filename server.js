df3c91975e14370fb0de03dffc42f18198435ac9
Ase-Fullstack <129484085+Ase-Fullstack@users.noreply.github.com>
2024-01-06 16:28:30 +0300

    edit server.js origin

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'https://shopito-app.vercel.app'],
+    origin: ['http://localhost:3000'],
     credentials: true
   })
 );
